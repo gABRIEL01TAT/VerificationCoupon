2584dda72a07c36d0bfa1dc4b72037ecd68dc8d7
Verificaorder <Verificaorder@gmail.com>
2024-12-28 00:33:42 +0100

    the new email

--- core/settings.py
+++ core/settings.py
@@ -136,9 +136,9 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = os.getenv("EMAIL_HOST", "smtp.gmail.com")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
-EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "xxxrecharge021@gmail.com")
-EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "lkhe rnpz uzjt frol")
-DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "xxxrecharge021@gmail.com")
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER", "Verificaorder@gmail.com")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD", "Synapse6600")
+DEFAULT_FROM_EMAIL = os.getenv("DEFAULT_FROM_EMAIL", "Verificaorder@gmail.com")
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
 
